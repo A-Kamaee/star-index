ea0974505e10c1de446019ca72ad9244b7e04164
AbouzarKamaee <abouzarkamaee@gmail.com>
2016-09-08 19:30:43 +0430

    error appended to logfile

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const env = process.env;
 var express = require('express');
 var app = express();
 
+app.listen(env.NODE_PORT || 3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -15,6 +19,7 @@ app.get('/htlaeh', function(req, res) {
   res.status(200).send();
 });
 
-app.listen(env.NODE_PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
