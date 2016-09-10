bbab1c14cebce64188fbc28ad20688e4b3d002a1
AbouzarKamaee <abouzarkamaee@gmail.com>
2016-09-10 12:34:02 +0430

    express.js added and configured based on openshift infrastructure

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const env = process.env;
 var express = require('express');
 var app = express();
 
-app.listen(env.NODE_PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -19,7 +15,37 @@ app.get('/htlaeh', function(req, res) {
   res.status(200).send();
 });
 
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
